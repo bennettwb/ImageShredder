bb99a4be9c97560eca26a1368152e45ec314d10c
mhamrah <mhamrah@gmail.com>
2011-05-06 16:22:57 -0400

    Using CSS3 Box Layout and added polling

--- WPD.Web/js/script.js
+++ WPD.Web/js/script.js
@@ -2,14 +2,41 @@
 
 */
 var model = {
-  currentImages: ko.observableArray([])
-}
+  currentImages: ko.observableArray([]),
+  refreshEnabled: ko.observable('false'),
+};
+model.toggleRefresh= function() {
+    if(this.refreshEnabled() == 'true') {
+      this.refreshEnabled('false');
+      $('#images').stopTime();
+
+    }
+    else {
+      this.refreshEnabled('true');
+      $('#images').everyTime(1000, 'controlled', getNew);
+
+    }
+  };
+
+model.refreshText= ko.dependentObservable(function() {
+  if(this.refreshEnabled() == 'true')
+  {
+    return 'Refresh Enabled';
+  }
+  else {
+    return 'Refresh Disabled';
+  }
+
+}, model);
 
 ko.applyBindings(model);
 
 $(document).ready(function () {
-    $('#images').everyTime(1000, 'controlled', getNew);
- //   getNew();
+
+//  model.refreshEnabled= true;
+
+  // $('#images').everyTime(1000, 'controlled', getNew);
+  //   getNew();
 
 });
 
